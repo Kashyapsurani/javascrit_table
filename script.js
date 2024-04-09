aaf3b1d717e0e877a8806265221a86e2d89f7b01
kashyapsurani <kashyapsurani49@gmail.com>
2024-04-09 16:54:26 +0530

    Javascript_table

---
+++ script.js
@@ -0,0 +1,64 @@
+document.write(`
+
+<table border="1" >
+<thead>
+   <tr>
+    <th colspan="4">Cmpany Logo</th>
+   </tr>
+</thead>
+<tbody>
+   <tr >
+    <td>
+    <ul>
+        <li id="Home"><a href="#Home">Home</a></li>
+        <li><a href="#Product">Product</a></li>
+            <li><a href="#About Us">About Us</a></li>
+        <li><a href="#Contact">Contact</a></li>
+    </ul>
+    </td>
+    <td>
+        <table>
+        <tr colspan="2" id="Product">
+            <td >Main Contect Area:</td>
+            <tR>
+            <td>This is the main content of the website</td>
+        </tR>
+        </tr>
+        </tbody>
+        </table>
+    </td>
+    <td>
+        <table  border="1">
+            <tr  id="About Us">
+                <td  height="60">sidebar:<br/>Additional information or linkd.</td>
+                <tr>
+                    <td>
+                </tr>
+               <tr>
+                <td height="60">featured Content:<br/> Highhting important information</td>
+                </tr>    
+                
+                
+            </tr>
+        </table >
+    </td>
+    <td>
+        <table  >
+            <tr id="Contact">
+                <td >Advertisement:</td>
+                <tr>
+                <td>My Phone Number:9016256121 </td>
+                </tr>    
+                
+                
+            </tr>
+       
+        </table >
+    </td>
+ <tr>
+    <td colspan="4">&copy 2023 Company name</td>
+ </tr>
+</tbody>
+</table>
+
+`)
